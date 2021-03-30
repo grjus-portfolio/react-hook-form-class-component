24e8f74202cc63c71b91411c10027b0375ea3f29
grjus <grjus_services+github@pm.me>
2021-03-30 23:23:36 +0200

    Initial configuration

---
+++ src/style.js
@@ -0,0 +1,12 @@
+import {createGlobalStyle} from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+body{
+    text-align:center;
+    font-family: 'Roboto', sans-serif;
+    font-size:16px;
+    color:rgba(0,0,0,0.8)
+}
+
+`
